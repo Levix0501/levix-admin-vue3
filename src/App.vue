395e88725a9496bf416efe396d239534c1638413
Levix <1176891456@qq.com>
2024-07-26 10:17:35 +0800

    feat: 新增 github 地址和 Nextjs 版本地址

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
+import { Github } from 'lucide-vue-next'
 import { appConfig } from './@config/appConfig'
 </script>
 
 <template>
-  <main className="h-screen flex justify-center pt-10">
-    <div className="space-y-4">
+  <main class="h-screen flex justify-center pt-10">
+    <div class="space-y-4">
+      <div class="flex space-x-4">
+        <a
+          href="https://github.com/Levix0501/levix-admin-vue3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github :size="24" />
+        </a>
+
+        <a href="https://next-admin.fecoder.cn" target="_blank" rel="noopener noreferrer">
+          Nextjs 版本
+        </a>
+      </div>
+
       <div>
         <p>Logo</p>
         <component :is="appConfig.logo" />
